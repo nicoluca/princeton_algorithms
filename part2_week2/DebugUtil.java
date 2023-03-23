e2699862cc79e719b895767d2742d459e549026d
Nico <nico.pfau@icloud.com>
2023-03-23 17:49:57 +0100

    Part II week 2: Cleaned up and passed with 94/100

--- part2_week2/DebugUtil.java
+++
@@ -1,9 +0,0 @@
-import java.util.Arrays;
-
-public class DebugUtil {
-    public static void printMatrix(double[][] matrix) {
-        System.out.println("Printing matrix with " + matrix.length + " rows and " + matrix[0].length + " columns.");
-        for (double[] row : matrix)
-            System.out.println(Arrays.toString(row));
-    }
-}
